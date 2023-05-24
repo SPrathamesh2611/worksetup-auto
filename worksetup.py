4e2e949bf6ec89afdcc1219390e0192dcee4655b
SPrathamesh2611 <134413659+SPrathamesh2611@users.noreply.github.com>
2023-05-24 05:32:47 +0530

    Update worksetup.py
    added comment to the code

--- worksetup.py
+++ worksetup.py
@@ -1,16 +1,26 @@
-#work setup automation
-
 import webbrowser as wb
 import os
 
 def workauto():
+    # Path to the code editor executable
     codepath = "E:\\programs\\Microsoft VS Code\\Code.exe"
+    
+    # Launch the code editor
     os.startfile(codepath)
+
+    # Path to the Google Chrome executable
     Chrome_path = 'C:/Users/hp/AppData/Local/Google/Chrome/Application/chrome.exe %s'
-    URLS = ("google.com",
-            "gmail.com",
-            "github.com/SPrathamesh2611")
+
+    # List of URLs to be opened in Google Chrome
+    URLS = (
+        "google.com",
+        "gmail.com",
+        "github.com/SPrathamesh2611"
+    )
+
+    # Open each URL in Google Chrome
     for url in URLS:
         wb.get(Chrome_path).open(url)
 
+# Call the workauto function to initiate the automation
 workauto()
